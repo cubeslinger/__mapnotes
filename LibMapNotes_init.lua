9da1620352da1c5912ff84f550fa2e5f53945126
Marco BARBIERI <marco.barbieri@unimore.it>
2019-06-26 10:45:41 +0200

    - added missing files to git repository.

---
+++ LibMapNotes_init.lua
@@ -0,0 +1,10 @@
+--
+-- Addon       LibMapNotes_init.lua
+-- Author      marcob@marcob.org
+-- StartDate   26/06/2019
+--
+
+-- Main initialization code for the library.
+
+if not Library then Library = {} end
+if not Library.LibMapNotes then Library.LibMapNotes = {} end
